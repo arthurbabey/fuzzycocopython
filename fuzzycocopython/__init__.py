7a8c92db0dde19b49e3eeafe75e28bb1ef015e94
Arthur Babey <arthur.babey@heig-vd.ch>
2025-03-06 15:44:31 +0100

    expose .so

--- fuzzycocopython/__init__.py
+++ fuzzycocopython/__init__.py
@@ -1,3 +1,6 @@
+# load the compiled c++ extension
+from . import fuzzycoco_core
+
 # simplifying basic import
 from .fuzzycoco_base import FuzzyCocoBase
 from .fuzzycoco_classifier import FuzzyCocoClassifier
