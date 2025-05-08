fbaf50e77f8087751bde0819743b4543921b43f4
Arthur Babey <arthur.babey@heig-vd.ch>
2025-05-08 14:16:31 +0200

    drop old fuzzycoco in prep for submodule

--- fuzzycoco/src/duktape/duktape.pri
+++
@@ -1,8 +0,0 @@
-INCLUDEPATH += $$PWD
-
-HEADERS += \
-    $$PWD/duk_config.h \
-    $$PWD/duktape.h
-
-SOURCES += \
-    $$PWD/duktape.c
--- fuzzycoco/src/genome.h
+++
@@ -1,9 +0,0 @@
-#ifndef GENOME_H
-#define GENOME_H
-
-#include "bitarray.h"
-
-using Genome = BitArray;
-using Genomes = vector<Genome>;
-
-#endif // GENOME_H
